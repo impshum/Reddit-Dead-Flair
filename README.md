013f9cdfce413d59dcf37834c3a07c476285f35f
impshum <impshum@impshum.co.uk>
2023-01-30 17:26:43 +0545

    v1

---
+++ README.md
@@ -0,0 +1,31 @@
+## Reddit Dead Flairing Thing
+
+A bot that posts images to many subreddits at certain times of the day.
+
+![](ss.jpg)
+
+### Install & Run Instructions
+
+1.  Install Python https://www.python.org/ (make sure to add to PATH during install).
+2.  Open up a command prompt and `cd` (change directory) to where you unzipped the bot files.
+3.  Install requirements `pip install -r requirements.txt`.
+4.  Create Reddit (script) app at https://www.reddit.com/prefs/apps/ and get client_id and client_secret.
+5.  Edit `conf.ini` with your details and settings.
+6.  Run it `python run.py`.
+
+### Conf.ini Instructions
+
+    [REDDIT]
+    reddit_user = YOUR REDDIT USERNAME
+    reddit_pass = YOUR REDDIT PASSWORD
+    reddit_client_id = YOUR REDDIT CLIENT ID
+    reddit_client_secret = YOUR REDDIT CLIENT SECRET
+
+    [SETTINGS]
+    max_reports = MAX NUMBER OF REPORTS BEFORE ADDING FLAIR
+    flair_text = FLAIR TEXT
+
+
+### Notes
+
+-   I will not be held responsible for any bad things that might happen to you or your Reddit accounts whilst using this bot. Follow Reddiquette and stay safe.
---
+++ run.py
@@ -0,0 +1,90 @@
+import praw
+import configparser
+import sqlite3
+from sqlite3 import Error
+
+
+config = configparser.ConfigParser()
+config.read('conf.ini')
+reddit_user = config['REDDIT']['reddit_user']
+reddit_pass = config['REDDIT']['reddit_pass']
+reddit_client_id = config['REDDIT']['reddit_client_id']
+reddit_client_secret = config['REDDIT']['reddit_client_secret']
+reddit_target_subreddit = config['REDDIT']['reddit_target_subreddit']
+max_reports = int(config['SETTINGS']['max_reports'])
+flair_text = config['SETTINGS']['flair_text']
+
+reddit = praw.Reddit(
+    username=reddit_user,
+    password=reddit_pass,
+    client_id=reddit_client_id,
+    client_secret=reddit_client_secret,
+    user_agent='Reddit Dead Flairing Thing (by u/impshum)'
+)
+
+
+def db_connect():
+    try:
+        conn = sqlite3.connect('data.db')
+        create_table = """CREATE TABLE IF NOT EXISTS posts (
+                                        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        submission_id TEXT NOT NULL,
+                                        author TEXT NOT NULL
+                                        );"""
+        conn.execute(create_table)
+        create_table = """CREATE TABLE IF NOT EXISTS dead_posts (
+                                        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        submission_id TEXT NOT NULL
+                                        );"""
+        conn.execute(create_table)
+        return conn
+    except Error as e:
+        print(e)
+    return None
+
+
+def insert_row(conn, submission, submission_id, author):
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM dead_posts WHERE submission_id = ? LIMIT 1", (submission_id,))
+    result = cur.fetchone()
+
+    if not result:
+        cur.execute("SELECT * FROM posts WHERE submission_id = ? AND author = ? LIMIT 1", (submission_id, author))
+        result = cur.fetchone()
+
+        if not result:
+            conn.execute("INSERT INTO posts (submission_id, author) VALUES (?, ?);", (submission_id, author))
+            conn.commit()
+
+        cur.execute("SELECT COUNT(submission_id) FROM posts WHERE submission_id = ?", (submission_id,))
+        result = cur.fetchone()
+
+        if result[0] == max_reports:
+            print(f'Flairing...')
+            conn.execute("INSERT INTO dead_posts (submission_id) VALUES (?);", (submission_id,))
+            conn.commit()
+
+            submission.mod.flair(text=flair_text)
+
+
+def read_db(conn):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+
+def main():
+    conn = db_connect()
+
+    for submission in reddit.subreddit(reddit_target_subreddit).new(limit=None):
+        for comment in submission.comments:
+            comment_body = comment.body
+            if '!dead' in comment_body:
+                insert_row(conn, submission, submission.id, comment.author.name)
+
+
+if __name__ == '__main__':
+    main()
